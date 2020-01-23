c64c437eab79696bb38b70d34db379cc2df2c7d0
Han Yang <ilovebabycat2002@hotmail.com>
2020-01-22 20:13:46 -0600

    comment added

--- climate_app.py
+++ climate_app.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 import datetime as dt
+# Establish Sqlalchemy reflection 
 engine = create_engine('sqlite:///Resources/hawaii.sqlite')
 Base = automap_base()
 Base.prepare(engine, reflect=True)
